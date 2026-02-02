9b41cb7166009137b93852098bd11b3676189a6b
24-Harshdeep <harshdeep24@navgurukul.org>
2026-02-02 22:49:42 +0530

    Fix API serverless function for Vercel

--- api/server.js
+++ api/server.js
@@ -1,13 +1,16 @@
 const jsonServer = require('json-server')
 const auth = require('json-server-auth')
+const path = require('path')
 
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+module.exports = (req, res) => {
+  const server = jsonServer.create()
+  const router = jsonServer.router(path.join(__dirname, 'db.json'))
+  const middlewares = jsonServer.defaults()
 
-server.db = router.db
-server.use(middlewares)
-server.use(auth)
-server.use(router)
-
-module.exports = server
+  server.db = router.db
+  server.use(middlewares)
+  server.use(auth)
+  server.use(router)
+  
+  server(req, res)
+}
